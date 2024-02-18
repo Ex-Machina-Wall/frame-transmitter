74955ce2b7efc83c337cf970f286ba455f02f5c9
Trevor O'Leary <git@trevoroleary.com>
2024-02-17 21:48:23 -0800

    added a test in frame_transmitter.py

--- ex_wall_frame_transmitter/frame_transmitter.py
+++ ex_wall_frame_transmitter/frame_transmitter.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from ex_wall_frame_transmitter.websocket_handler import WebsocketTransmitter
 from ex_wall_frame_transmitter.frame_formatter import Frame
@@ -18,3 +20,25 @@ class FrameTransmitter:
         frame = Frame(np_frame=np_frame)
         data_frame = frame.get_bytes()
         self._websocket_handler.set_latest_bytes(data_frame=data_frame)
+
+
+def main():
+    import logging
+    logging.getLogger("main")
+    logging.basicConfig(level="DEBUG")
+    from ex_wall_frame_transmitter.constants import WIDTH, HEIGHT
+    from decouple import config
+    destination_uri = config("DESTINATION_URI")
+    frame_transmitter = FrameTransmitter(destination_uri=destination_uri)
+    frame_transmitter.start()
+    fake_frame = np.zeros((HEIGHT, WIDTH, 3), dtype=int)
+    for h in range(HEIGHT):
+        for w in range(WIDTH):
+            fake_frame[h, w] = [0, 0, 0]
+    frame_transmitter.send_numpy_frame(np_frame=fake_frame)
+    time.sleep(1)
+    frame_transmitter.stop()
+
+
+if __name__ == "__main__":
+    main()
